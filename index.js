eff70749c8f5abd2a310e908682db87ee80e5d5c
Chloe Peng <chloepeng331@icloud.com>
2022-02-03 20:14:09 -0500

    commit tree 3598da88a80e0e50c892da2d1b6b53845a445c37

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-function saturdayFun(activiy ='roller-skate'){
-    return `This Saturday, I want to ${activiy}!`;
+function saturdayFun (activity= 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
 }
-
-
 
 function mondayWork (activity='go to the office'){
-    return `This Monday, I will ${activity}.`;
+    return `This Monday, I will ${activity}.`
 }
 
-
-
-function wrapAdjective(msg,  message='You are '){
-    const innerFunction = function (worker = 'a hard worker'){
-        return `${message}${msg}${worker}${msg}!`;
+function wrapAdjective (special, msg = 'You are '){
+    const inner = function (hardWorker='a hard worker') {
+        return `${msg}${special}${hardWorker}${special}!`;
     };
-    return innerFunction;
+    return inner;
 }
+
+
+
+
+
+
+
+
+// // function wrapAdjective(msg,  message='You are '){
+// //     const innerFunction = function (worker = 'a hard worker'){
+// //         return `${message}${msg}${worker}${msg}!`;
+// //     };
+// //     return innerFunction;
+// // }
